f2f6ea8025232dd4dbc30ca07901bf121da6b352
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-14 14:42:37 -0700

    task1

---
+++ utils/db.js
@@ -0,0 +1,44 @@
+import { env } from 'process';
+import { MongoClient, ObjectId } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    let host;
+    if (env.DB_HOST) {
+      host = env.DB_HOST;
+    } else {
+      host = 'localhost';
+    }
+    let port;
+    if (env.DB_PORT) {
+      port = env.DB_PORT;
+    } else {
+      port = 27017;
+    }
+    let database;
+    if (env.DB_DATABASE) {
+      database = env.DB_DATABASE;
+    } else {
+      database = 'files_manager';
+    }
+    const client = new MongoClient(`mongodb://${host}:${port}/${database}`);
+    this.myClient.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    usersCollection = this.client.db().collection('users');
+    return usersCollection.countDocuments();
+  }
+
+  async nbFiles() {
+    const filesCollection = this.client.db().collection('files');
+    return filesCollection.countDocuments();
+  }
+
+  const dbClient = new DBClient();
+  export default dbClient;
+}

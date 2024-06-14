99e30dc535885ceaa9c504701f46662d48af1ee5
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-14 08:23:40 -0700

    task0

---
+++ utils/redis.js
@@ -0,0 +1,31 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => console.log(err));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.GET).bind(this.client);
+    return getAsync(key)
+  }
+
+  async set(key, val, dur) {
+    const setAsync = promisify(this.client.SET).bind(this.client);
+    return setAsync(key, val, 'EX', time);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.DEL).bind(this.client);
+    return delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient;
+export default redisClient;

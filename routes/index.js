e945eca6e7ab6cfb9192799a1e15de072935d661
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-14 15:56:05 -0700

    routes

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+
+const router = Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+module.exports = router;

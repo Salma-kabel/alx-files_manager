d3c292fa1fe242ff0b3c9bbe3b093edda023bce3
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-14 16:10:46 -0700

    style fix

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express';
 const app = express();
 const port = env.PORT || 5000;
 const routes = require('./routes/index');
+
 app.use(express.json());
 app.use(routes);
 app.listen(port, '127.0.0.1');

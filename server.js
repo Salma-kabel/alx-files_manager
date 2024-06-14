76ae49e6476ae8c5232529459e8c0103af25cccb
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-14 15:55:38 -0700

    server

---
+++ server.js
@@ -0,0 +1,11 @@
+import { env } from 'process';
+import express from 'express';
+
+const app = express();
+const port = env.PORT || 5000;
+const routes = require('./routes/index');
+app.use(express.json());
+app.use(routes);
+app.listen(port, '127.0.0.1');
+
+export default app;

12b953b7f17ebc09d46ad47fad771b3758594d0e
THEKINGSTAR <thekingstar5002@gmail.com>
2024-06-20 05:04:17 +0300

    IN LINE 13 CORRECT 'UsersController'

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,6 @@ router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UserController.getMe);
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;

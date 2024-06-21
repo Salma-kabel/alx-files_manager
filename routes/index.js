eb4fd1c205983826be2468cb3478a77db8d47604
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-21 09:35:52 -0700

    task 4 edit

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,12 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UserController.getMe);
 
 module.exports = router;
